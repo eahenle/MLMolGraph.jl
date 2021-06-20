7bd021b585ead170b9e8c0198443ea7d190d6b49
Adrian Henle <eahenle@gmail.com>
2021-06-20 12:42:48 -0700

    correction

--- docs/make.jl
+++ docs/make.jl
@@ -8,9 +8,9 @@ makedocs(
     clean = true,
     pages = [
             "MLMolGraph" => "index.md",
-            "Graphs and AI" => "graph_ml.md",
-            "Simple Execution" => "process_crystals.md",
-            "API" => "api.md"
+            "Graphs & AI" => "graph_ml.md",
+            "Processing" => "process_crystals.md",
+            "API Docs" => "api.md"
             ],
     format = Documenter.HTML(assets = ["assets/flux.css"])
 )
